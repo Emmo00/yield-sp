d5191e13265ef5f19fe1fec1cc2010264f517ac7
Emmo00 <nwaforemmanuel005@gmail.com>
2026-03-31 19:40:35 +0100

    feat: add ERC20 and LoanVault ABIs, and define contract addresses for mainnet and testnet

---
+++ app/lib/constants.ts
@@ -0,0 +1,11 @@
+export const CONTRACT_ADDRESSES = {
+    mainnet: {
+        'loan-vault': '0x1234567890abcdef1234567890abcdef12345678',
+        'stablecoin': '0xabcdef1234567890abcdef1234567890abcdef12' // ESPEES Stablecoin address
+    },
+    testnet: {
+        'loan-vault': '0xabcdefabcdefabcdefabcdefabcdefabcdefabcd',
+        'stablecoin': '0x1234561234561234561234561234561234561234' // ESPEES Stablecoin address on testnet
+    }
+}
+
